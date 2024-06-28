fdd754d525976dffc2dcb63cd65d26056cdbec3c
cristiano-c <cristianocavo@me.com>
2024-06-29 01:04:28 +0200

    feat: installed javadocs and generated apidocs

---
+++ apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.acme.hip.hop.chocobo"},{"l":"com.acme.hip.hop.chocobo.geometry"},{"l":"com.acme.hip.hop.chocobo.rest"}];updateSearchResults();

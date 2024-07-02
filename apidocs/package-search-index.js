542ff92207ae06628d6d842ddd69361a8232846d
cristiano-c <cristianocavo@me.com>
2024-07-03 00:55:27 +0200

    feat: updated apidocs

--- apidocs/package-search-index.js
+++ apidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.acme.hip.hop.chocobo"},{"l":"com.acme.hip.hop.chocobo.geometry"},{"l":"com.acme.hip.hop.chocobo.rest"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.acme.hip.hop.chocobo"},{"l":"com.acme.hip.hop.chocobo.config"},{"l":"com.acme.hip.hop.chocobo.geometry"},{"l":"com.acme.hip.hop.chocobo.rest"}];updateSearchResults();
